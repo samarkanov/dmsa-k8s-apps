d328b2771436965dd8e7ce552963e01a72e852b1
Dmitry Samarkanov <samarkanov@gmail.com>
2024-11-02 18:16:38 +0000

    updated my app

--- simple/src/app.py
+++ simple/src/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a route for the root URL ('/')
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! Here is some good news for you...'
 
 # Run the server
 if __name__ == '__main__':
